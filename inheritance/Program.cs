3e8cf0294c1b56ba2e3ae2ea0af97c8e2d8ad255
Mohamed Saleem <mohamedselim701@gmail.com>
2022-07-22 09:24:26 +0200

    Create Inheritance console app

---
+++ inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+The main four pillars of OOP: 
+    Inheritance
+    encapsulation
+    Abstraction
+    polymorphism
+
+Inheritance: is a feature of oop which simply allows of the creation of 
+ a new class of existing class by inheriting its atributes and behaviors.
+
+base class: Iherited from
+derived class: inheriting from it
+
+class: You can create objects from
+interface: Is similar to abstract class but without methods implementions
+    but it's cpu frindly
+*/
